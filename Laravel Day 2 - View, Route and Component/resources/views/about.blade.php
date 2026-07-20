97e8a35e0e793260e57d2b25e94cbb2444097df0
Dhan Raj Gurung <drgurung82@gmail.com>
2026-07-20 14:17:20 +0545

    Upload Laravel project

---
+++ Laravel Day 2 - View, Route and Component/resources/views/about.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script src="https://cdnjsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <style>
+        .container {
+            width: 90%;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+
+<body class="bg-purple-50">
+
+    <x-header/>
+
+    <main></main>
+
+    <footer></footer>
+</body>
+
+</html>
---
+++ Laravel Day 2 - View, Route and Component/routes/web.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/courses', function () {
+    return view('courses');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
