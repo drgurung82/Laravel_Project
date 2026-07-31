9f063627b859ed4cd862f6c93275bda16962375f
Dhan Raj Gurung <drgurung82@gmail.com>
2026-07-31 19:45:22 +0545

    Update project files

--- Laravel Day 7 - Controller, Relationship and Admission Create/app/Http/Controllers/PageController.php
+++ Laravel Day 7 - Controller, Relationship and Admission Create/app/Http/Controllers/PageController.php
@@ -5,15 +5,19 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 class PageController extends Controller
+
+// Function to open welcome page
 {
     public function welcome() {
     return view("welcome");
 }
 
+// Function to open about page
 public function about() {
     return view("about");
 }
 
+// Function to open services page
 public function services() {
     return view("services");
 }
